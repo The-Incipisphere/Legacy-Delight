ed8184a13caed53ad2b1306e595f3b8b9d60838e
TheDogOfChaos <46350316+TheDogOfChaos@users.noreply.github.com>
2025-12-17 04:26:34 +0000

    WE HAVE A BLANK THING INSTEAD OF A MISSING TEXTURE, HELL YEAH

--- src/main/java/io/thedogofchaos/legacydelight/common/blocks/BlockChoppingBoard.java
+++ src/main/java/io/thedogofchaos/legacydelight/common/blocks/BlockChoppingBoard.java
@@ -7,6 +7,8 @@ import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
+import static com.gtnewhorizon.gtnhlib.client.model.ModelISBRH.JSON_ISBRH_ID;
+
 public class BlockChoppingBoard extends BlockContainer {
     public BlockChoppingBoard() {
         super(Material.wood);
@@ -20,5 +22,10 @@ public class BlockChoppingBoard extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileEntityChoppingBoard();
+    }
+
+    @Override
+    public int getRenderType() {
+        return JSON_ISBRH_ID;
     }
 }
