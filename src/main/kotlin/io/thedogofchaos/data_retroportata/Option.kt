5fc0e9520be03dabd03ff91ac5b825dca169c5a2
TheDogOfChaos <46350316+TheDogOfChaos@users.noreply.github.com>
2026-02-16 15:44:07 +0000

    clean-up on aisle Option

--- src/main/kotlin/io/thedogofchaos/data_retroportata/Option.kt
+++ src/main/kotlin/io/thedogofchaos/data_retroportata/Option.kt
@@ -2,15 +2,28 @@ package io.thedogofchaos.data_retroportata
 
 // WELCOME TO RUST, BUCKOS
 @JvmInline
-value class Option<out T> internal constructor(internal val value: Any?) {
+value class Option<out T> internal constructor(internal val value: Any) {
     companion object {
+        /**
+         * Used to deliberately represent presence of a given thing.
+         *
+         * **Cannot be set as null.**
+         */
         fun <T : Any> some(value: T): Option<T> = Option(value)
+
+        /**
+         * Used to deliberately represent absence of a given thing.
+         */
         fun <T> none(): Option<T> = Option(makeNone())
     }
 
     val isSome: Boolean get() = value !is None
     val isNone: Boolean get() = !isSome
 
+    /**
+     * Assertively grabs the value inside an [Option].
+     * @throws NoSuchElementException If the Option contains None.
+     */
     @Suppress("UNCHECKED_CAST")
     fun unwrap(): T = when {
         isSome -> value
